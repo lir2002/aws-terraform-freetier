4a83687ebd14eaf532d072b3aecdc2e4d91ba194
Ethan Li <Ethan_Li@epam.com>
2023-02-05 12:33:31 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+data "aws_key_pair" "East2" {
+  key_name           = "East2ED25519"
+  include_public_key = true
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05bfbece1ed5beb54"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraformAmi-Feb2"
+  }
+
+  key_name = data.aws_key_pair.East2.key_name
+
+  vpc_security_group_ids = ["sg-0ef47b71cfc237432"]
+
+  subnet_id = "subnet-0a7a150f50e916932"
+}
